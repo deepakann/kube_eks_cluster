83c8c2f591a876ef9e7571a455320b01c547d10c
Deepa Kannaiyan <deepakann77@gmail.com>
2025-02-02 12:23:49 +0530

    Update nodegroup.tf

--- module/eks/nodegroup.tf
+++ module/eks/nodegroup.tf
@@ -1,6 +1,5 @@
-resource "aws_iam_role" "global-node-group-role" {
-  name = "node-group-role"
-
+resource "aws_iam_role" "node-group-role" {
+  name = "eks-nodegroup"
   assume_role_policy = jsonencode({
     Statement = [{
       Action = "sts:AssumeRole"
@@ -15,33 +14,32 @@ resource "aws_iam_role" "global-node-group-role" {
 
 resource "aws_iam_role_policy_attachment" "global-eks-worker-policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.global-node-group-role.name
+  role       = aws_iam_role.node-group-role.name
 }
 
 resource "aws_iam_role_policy_attachment" "global-eks-cni-policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.global-node-group-role.name
+  role       = aws_iam_role.node-group-role.name
 }
 
 resource "aws_iam_role_policy_attachment" "global-eks-container-policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.global-node-group-role.name
+  role       = aws_iam_role.node-group-role.name
 }
 
-
-resource "aws_eks_node_group" "global-node-group" {
-  cluster_name = aws_eks_cluster.global-cluster.name
-  node_group_name = "HighIn"
-  node_role_arn = aws_iam_role.global-node-group-role.arn
-  subnet_ids = [aws_subnet.pubsub01.id, aws_subnet.pubsub02.id] #aws_subnet.pri01.id, aws_subnet.pri02.id]
+resource "aws_eks_node_group" "instance-node-group" {
+  cluster_name  = aws_eks_cluster.eksmaster.name
+  node_group_name = "WorkerNG"
+  node_role_arn = aws_iam_role.node-group-role.arn
+  subnet_ids = [aws_subnet.pubsub01.id, aws_subnet.pubsub02.id]
   instance_types = ["t2.micro"]
   scaling_config {
     desired_size = 2
-    max_size = 2
-    min_size = 2
+    max_size     = 2
+    min_size     = 2
   }
   labels = {
-    zone = "west"
+    zone = "east"
   }
   depends_on = [
     aws_iam_role_policy_attachment.global-eks-cni-policy,
