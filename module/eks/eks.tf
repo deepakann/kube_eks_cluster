d9986481ef3b50bc44cbc02681fb0532b9161b64
Deepa Kannaiyan <deepakann77@gmail.com>
2025-02-02 12:21:44 +0530

    Update eks.tf

--- module/eks/eks.tf
+++ module/eks/eks.tf
@@ -1,13 +1,13 @@
-resource "aws_eks_cluster" "global-cluster" {
-  name = var.clustername
-  role_arn = aws_iam_role.globalrole.arn
-  version = "1.30"
+resource "aws_eks_cluster" "eksmaster" {
+  name = var.ekscluster
+  role_arn = aws_iam_role.master.arn
+  version = "1.31"
 
   vpc_config {
-    subnet_ids = [aws_subnet.pubsub01.id, aws_subnet.pubsub02.id] #aws_subnet.pri01.id, aws_subnet.pri02.id]
+    subnet_ids = [aws_subnet.pubsub01.id, aws_subnet.pubsub02.id] 
   }
   depends_on = [
-    aws_iam_role.globalrole
+    aws_iam_role.master
   ]
   tags = {
     Name = "clustertype"
